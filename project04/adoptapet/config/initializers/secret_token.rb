520e528025b4175c4952955756fb3c3f60dde6e1
krobison <krobison@mines.edu>
2014-02-04 21:13:15 -0700

    Reupload

--- project04/adoptapet/config/initializers/secret_token.rb
+++ project04/adoptapet/config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Adoptapet::Application.config.secret_key_base = '793f98553dbd607600d60e695ce4dd983edb8d920b4b44fd3532d37ff87bf784b5d374daca6648a5b14d2728e59269d7dd680421ae95baf73e753e0e0e3b04fc'
+Adoptapet::Application.config.secret_key_base = '1526167d574c8f72e68ae405f98c4a2b365f8893380a1679ed05e9b0e10844916d0aead1e32bb8e883442075ac7e0f0b6135c6cda2300dceac71ad28d85a7c80'
