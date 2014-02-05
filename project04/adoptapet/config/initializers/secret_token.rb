419e49e2dc2ed0363c624d767dce529ad374ae87
krobison <krobison@mines.edu>
2014-02-04 20:59:21 -0700

    Added skeleton for unit4

---
+++ project04/adoptapet/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adoptapet::Application.config.secret_key_base = '793f98553dbd607600d60e695ce4dd983edb8d920b4b44fd3532d37ff87bf784b5d374daca6648a5b14d2728e59269d7dd680421ae95baf73e753e0e0e3b04fc'
